7eaeb76439bb6899ade834efd1226800bbca5b2e
SolgunTT <solgun.tveit@gokstadakademiet.nogit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.name ditt>
2023-03-15 10:15:17 +0100

    første innsending

---
+++ Library/Mobile Documents/com~apple~CloudDocs/.Trash/JS2_Arbeidskrav/js/script.js
@@ -0,0 +1,95 @@
+const milkDiv = document.getElementById("milk");
+
+const milkP = document.querySelector("#milk p").innerHTML = `Melk ${26},-`;
+
+  
+  
+
+const breadDiv = document.getElementById("bread");
+
+const breadP = document.querySelector("#bread p").innerHTML = `Brød ${27},-`;
+
+  
+
+// milkDiv.addEventListener("click", addToSC);
+
+  
+
+let products = [`Melk ${26},-`, `Brød ${27},-`];
+
+  
+
+console.log(products);
+
+  
+  
+
+const shoppingCart = document.querySelector(".shoppingCart").innerHTML =
+
+`<tr class="headline"><td>Vare</td>
+
+<td>Beløp</td>
+
+<td>Slett</td></tr>
+
+<tr>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td></tr>
+
+<tr>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td></tr>
+
+<tr>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td></tr>
+
+<tr>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td></tr>
+
+<tr>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td></tr>
+
+<tr>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td>
+
+<td>dgfdg</td></tr>`
+
+  
+  
+
+// function addToSC {
+
+  
+
+// for ()
+
+  
+  
+
+// }
